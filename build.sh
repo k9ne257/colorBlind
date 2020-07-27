efb742e9984b8c14dc69ef8d4fa05f7f881f7724
k9ne257 <unelteed@hotmail.com>
2020-07-27 03:55:01 +0200

    init main.c

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -o colorBlind main.c
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc,char* argv[]){
+	printf("please enter enter your HEX color\n");
+	char hexcode [20] ;
+	scanf("%s", hexcode);
+	printf("%s\n", hexcode);
+	
+	return 0;
+}
